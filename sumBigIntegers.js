51343c17aecff310e3af7f63b333584c84d7dea6
vlada9597 <vladazoloto@icloud.com>
2025-05-05 02:02:26 +0300

    Add files via upload

---
+++ sumBigIntegers.js
@@ -0,0 +1,19 @@
+/*
+ Створіть функцію sumBigIntegers, яка приймає два рядки (numStr1 та numStr2), що представляють великі числа.
+ Функція повинна перетворити ці рядки на BigInt і повернути їх суму.
+
+ console.log(sumBigIntegers('9007199254740991', '9007199254740991')); // виводить 18014398509481982n
+*/
+
+// function sumBigIntegers(numStr1, numStr2) {
+//   // 'your code here'
+//   return 'your code here'
+// }
+
+function sumBigIntegers(numStr1, numStr2) {
+  const bigInt1 = BigInt(numStr1);
+  const bigInt2 = BigInt(numStr2);
+  return bigInt1 + bigInt2;
+}
+
+console.log(sumBigIntegers('9007199254740995', '9007199254740991')); 
